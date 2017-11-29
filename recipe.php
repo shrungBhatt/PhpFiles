7a27baf73c5c95263d400f56d23e5b8e734b0a8c
Shrung Bhatt <shrungbhatt@gmail.com>
2017-11-29 23:55:53 +0530

    29/11/17

---
+++ recipe.php
@@ -0,0 +1,19 @@
+<?php
+require "conn.php";
+
+$ingredient = "Bananas,Apple,Grapes";//$_POST["ingredient"];
+
+$mysql_qry = "SELECT * FROM recpie_data WHERE ingredient LIKE '$ingredient';";
+
+$dataArray = array();
+$result = mysqli_query($conn, $mysql_qry);
+
+$index = 0;
+while($row = mysqli_fetch_assoc($result)){ // loop to store the data in an associative array.
+     $dataArray[$index] = $row;
+     $index++;
+}echo json_encode($dataArray);
+
+
+
+?>
