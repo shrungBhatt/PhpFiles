441b571af61f706f1bfaa341bc73e0eed4596a6e
Shrung Bhatt <shrungbhatt@gmail.com>
2017-11-30 12:45:17 +0530

    Updated conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
-$db_name = "ernexus";
-$mysql_username = "root";
-$mysql_password = "root";
+$db_name = "u349430138_root";
+$mysql_username = "u349430138_root";
+$mysql_password = "tomnjerry";
 $server_name = "localhost";
 
 $conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
