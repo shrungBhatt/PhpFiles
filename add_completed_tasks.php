bdc687cdcdad6dd1451203b0c751d0ee47247a55
Shrung Bhatt <shrungbhatt@gmail.com>
2018-01-07 14:53:08 +0530

    Updated add_completed_task.php & Created fetch_completed_tasks.php

--- add_completed_tasks.php
+++ add_completed_tasks.php
@@ -5,9 +5,10 @@ $startDate = $_POST["start_date"];
 $endDate = $_POST["end_date"];
 $taskHeader = $_POST["task_header"];
 $taskContent = $_POST["task_content"];
+$repCounter = $_POST["rep_counter"]
 
 
-$mysql_qry = "insert into completed_task_data (start_date, end_date,task_header,task_content) values ('$startDate','$endDate','$taskHeader','$taskContent')";
+$mysql_qry = "insert into completed_task_data (start_date, end_date,task_header,task_content,rep_counter) values ('$startDate','$endDate','$taskHeader','$taskContent','$repCounter')";
 
 if($conn->query($mysql_qry) === TRUE){
 	echo "Added Completed task";
